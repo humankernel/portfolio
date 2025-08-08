67e66f303affaef03299117f8a21b7cc35556fdc
humankernel <rivasjoaquin02@gmail.com>
2025-08-08 18:07:02 -0400

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
+
+import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  site: 'https://yourdomain.com/',
+  integrations: [mdx(), sitemap()]
+});
