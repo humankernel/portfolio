0012ad668e4d728755c0ed3ab46e5423e91e38f0
strange <rivasjoaquin02@gmail.com>
2025-11-08 19:46:28 -0500

    fix: fixed some view transitions issues feat: setup prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: { defaultStrategy: "viewport" },
   vite: {
     plugins: [tailwindcss()]
   }
